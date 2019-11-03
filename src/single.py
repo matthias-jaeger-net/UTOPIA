6381c2231c963d107bafeac5bb9177fc56c166ad
Matthias Jäger <write@matthias-jaeger.net>
2019-11-03 13:03:43 +0100

    moved gitignore

--- src/single.py
+++ src/single.py
@@ -3,14 +3,11 @@ from colortools import Color
 from colortools import randomColor
 cols = 69
 page_color = randomColor()
-
 print('Test page running')
 print('#'*cols)
-
 page_color.analyze()
 page_color.log()
 print('#'*cols)
-
 print('Expected similar colors')
 print('#'*cols)
 similar = page_color.similar_colors()
@@ -18,5 +15,4 @@ for color in similar:
     print('-'*cols)
     page_color.analyze()
     color.log()
-
 print('#'*cols)
