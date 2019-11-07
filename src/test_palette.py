45a8fb9f2632150261c246569868f59d2aa4bad9
Matthias Jäger <write@matthias-jaeger.net>
2019-11-07 17:23:01 +0100

    fixed __pycache__ problem with restart

--- src/test_palette.py
+++ src/test_palette.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!usr/bin/env python
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Intending to get a color palette from a given single color
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
