54b0fe64d89176f9c8d3a31e65eacd4dd8ec34cc
Matthias Jäger <write@matthias-jaeger.net>
2019-11-07 17:15:29 +0100

    imports dont work any more bug

---
+++ src/test_palette.py
@@ -0,0 +1,5 @@
+#!usr/bin/env python3
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Intending to get a color palette from a given single color
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+from colortools import Color
